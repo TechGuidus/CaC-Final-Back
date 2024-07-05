1a2c9acfb27dd8aa255b4fcd99fcd39232f4d26c
MauriDemasi <mauridemasi88@gmail.com>
2024-07-05 10:05:42 -0300

    Revert "first commit"
    This reverts commit 9c9cc3a089c4a75843122949b57d677fd7fb1f05.

--- src/main/java/com/example/cac_final/repository/AutorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cac_final.repository;
-
-import com.example.cac_final.entity.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutorRepository extends JpaRepository<Autor, Long> {
-}
--- src/main/java/com/example/cac_final/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cac_final.repository;
-
-import com.example.cac_final.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
